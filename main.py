eb831edd1947f5ea5ba135c16aa333c86d9408dd
Marko Girlinger <marko.girlinger@gmail.com>
2021-10-07 21:08:10 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+mood = input("Today I feel ")
+
+if mood == "happy":
+    print("It's great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "hungry":
+    print("Have an orange and you'll be okay!")
+elif mood == "sad":
+    print("Smile, there's more reasons to be happy than sad.")
+elif mood == "excited":
+    print("Nice to see you so hyped!")
+elif mood == "relaxed":
+    print("A good rest sure helps.")
+else:
+    print("I don't know how to help you with that.")
